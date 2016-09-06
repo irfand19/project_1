ad43b62c23a473b4ce9669cd4ec33932f512c31b
irfand19 <irfand19@gmail.com>
2016-09-06 14:38:37 +0700

    fourth commit

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
 <title> coba if </title>
    <body>
     <?php
-         $coba ="shareeeeee";
+         $coba ="coba";
 
          if ($coba == "shareilmu") {
                 echo "SHARE ILMU itu sangat bermanfaat";
