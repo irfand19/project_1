4224ea3adae9c539822fcfcdaa8f5a9833d8773a
irfand19 <irfand19@gmail.com>
2016-09-06 14:10:45 +0700

    first commit

---
+++ home.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title> coba if </title>
+   <body>
+    <?php
+         $coba ="shareilmu";
+
+         if ($coba == "shareilmu") {
+                echo "SHARE ILMU itu sangat bermanfaat";
+          }
+         ?>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>coba if-else </title>
+</head>
+    <body>
+        <?php
+            $satu = "one";
+                 if ($satu=="one")
+                      echo "Satu adalah bahasa indonesia dari ONE";
+                 else {
+                      echo "Satu bukan bahasa indonesia dari ONe";
+                  }
+          ?>
+    </body>
+</html>
